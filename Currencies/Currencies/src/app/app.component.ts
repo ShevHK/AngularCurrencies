d093a65c12e8dfbcf851b37e4f1f4ec38ab32584
Sheva <hksheva@gmail.com>
2024-01-12 00:33:20 +0200

    minor fix

--- Currencies/Currencies/src/app/app.component.ts
+++ Currencies/Currencies/src/app/app.component.ts
@@ -1,21 +1,20 @@
 import { Component } from '@angular/core';
-import { ExchangeRate } from './components/services/ExchangeRate';
 import { CurrencyService } from './components/services/currency.service';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Project';
   currencies: string[] = ['UAH', 'EUR', 'USD'];
-  currenciesValues: GLfloat[] = [1, 0, 0];
-  selectedFrom: string = 'UAH';
-  selectedTo: string = 'EUR';
-  valueFrom: GLfloat = 0;
-  valueTo: GLfloat = 0;
+  currenciesValues: number[] = [1, 0, 0];
+  selectedFrom = 'UAH';
+  selectedTo = 'EUR';
+  valueFrom = 0;
+  valueTo = 0;
+
   constructor(private currencyService: CurrencyService) {
     this.fetchData();
   }
@@ -24,33 +23,33 @@ export class AppComponent {
     this.selectedFrom = option;
     this.onChangeTo(this.valueTo);
   }
+
   onSelectTo(option: string): void {
     this.selectedTo = option;
     this.onChangeFrom(this.valueFrom);
   }
+
   onArrowClick(): void {
-    var temp = this.selectedFrom;
-    this.selectedFrom = this.selectedTo;
-    this.selectedTo = temp;
-
-    var help = this.valueFrom;
-    this.valueFrom = this.valueTo;
-    this.valueTo = help;
-  }
-  onChangeFrom(valueFrom: GLfloat): void {
-    this.valueTo = valueFrom * this.CheckCurrency(this.selectedFrom, this.selectedTo);
-    this.valueTo.toFixed(2);
-  }
-  onChangeTo(valueTo: GLfloat): void {
-    this.valueFrom = valueTo * this.CheckCurrency(this.selectedTo, this.selectedFrom);
-    this.valueFrom.toFixed(2);
+    [this.selectedFrom, this.selectedTo] = [this.selectedTo, this.selectedFrom];
+    [this.valueFrom, this.valueTo] = [this.valueTo, this.valueFrom];
   }
 
-  CheckCurrency(symbol_1: string, symbol_2: string): GLfloat {
-    var result = this.currenciesValues[this.currencies.indexOf(symbol_1)] /
-      this.currenciesValues[this.currencies.indexOf(symbol_2)];
-    return result;
+  onChangeFrom(valueFrom: number): void {
+    this.valueTo = valueFrom * this.checkCurrency(this.selectedFrom, this.selectedTo);
+    this.valueTo = parseFloat(this.valueTo.toFixed(2));
   }
+
+  onChangeTo(valueTo: number): void {
+    this.valueFrom = valueTo * this.checkCurrency(this.selectedTo, this.selectedFrom);
+    this.valueFrom = parseFloat(this.valueFrom.toFixed(2));
+  }
+
+  private checkCurrency(symbol_1: string, symbol_2: string): number {
+    const index1 = this.currencies.indexOf(symbol_1);
+    const index2 = this.currencies.indexOf(symbol_2);
+    return this.currenciesValues[index1] / this.currenciesValues[index2];
+  }
+
   async fetchData(): Promise<void> {
     try {
       const exchangeRates = await this.currencyService.getExchangeRates().toPromise();
